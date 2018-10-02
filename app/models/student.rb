ebb5d594e1c328514bcff962a126335cee2e4dd6
Emanuel Bierman <emanuel.bierman@gmail.com>
2018-10-02 19:24:50 +0000

    automatically backed up by learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,5 @@
 class Student < ActiveRecord::Base
   def to_s
-    "#{self.first_name.capitalize} + #{self.last_name.capitalize}"
+    "#{self.first_name.capitalize} #{self.last_name.capitalize}"
   end
 end
